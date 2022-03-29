a4b9d28d6fa6deab96d45358e06e66ce16e8de1c
Simon <ronec.simon@gmail.com>
2022-03-29 17:06:24 +0200

    Add bookings APIs

--- mtaa_main.py
+++ mtaa_main.py
@@ -14,7 +14,74 @@ from mtaa_app.models import Users, Restaurants, Bookings
 
 dbs_psswd = "helloworld" #password for authorization
 
+def for_key_const(col, val):
+    if col == "id_bookings":
+        q = Bookings.objects.values("id_bookings")
+        q = q.filter(Q(id_bookings=val))
+        if q:
 
+            pass
+        else:
+            err = {"errors": col, "reasons": "booking does not exist"}
+            return err, 2
+    return {}, 1
+
+def book_insert_rest(i, val):
+    id_r = 0
+    try:
+        id_r = int(val)
+    except:
+        err = {"errors": i, "reasons": "needs to be a number"}
+        return 0, err, 422
+    q = Restaurants.objects.values("id_restaurants")
+    q = q.filter(Q(id_restaurants=val))
+    if q:
+        return id_r, {}, 200
+    else:
+        err = {"errors": i, "reasons": "restaurant does not exist"}
+        return 0, err, 422
+
+def book_insert_usr(i, val):
+    id_u = 0
+    try:
+        id_u = int(val)
+    except:
+        err = {"errors": i, "reasons": "needs to be a number"}
+        return 0, err, 422
+    q = Users.objects.values("id_users")
+    q = q.filter(Q(id_users=val))
+    if q:
+        return id_u, {}, 200
+    else:
+        err = {"errors": i, "reasons": "user does not exist"}
+        return 0, err, 422
+
+def book_insert_ppl(i, val):
+
+    try:
+        a = int(val)
+    except:
+        err = {"errors": i, "reasons": "needs to be a number"}
+        return 0, err, 422
+    if a <= 0:
+        err = {"errors": i, "reasons": "needs to be more than 0"}
+        return 0, err, 422
+    return a, {}, 200
+
+
+def book_insert_date(i, val):
+
+    try:
+        val = val.split("-")
+        val_1 = val[2].split(" ")
+        val_2 = val_1[1].split(":")
+        d = datetime(int(val[0]), int(val[1]), int(val_1[0]), int(val_2[0]), int(val_2[1]), int(val_2[2]))  # transformations into datetime
+
+    except:
+        err = {"errors": i, "reasons": "wrong format try: YYYY-MM-DD HH:MM:SS"}
+        return 0, err, 422
+
+    return d, {}, 200
 
 @csrf_exempt
 def user_reg(request): #register a new users
@@ -162,3 +229,164 @@ def rest_det(request, rest_id): #get info about a chosen restaurant
     else:
         err = {"errors": "method", "reasons": "unrecognized"}
         return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=405)
+
+
+@csrf_exempt
+def book_create(request): #user creates a new booking
+    if request.method == 'POST':
+        if request.POST.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        arr_of_cols = ["user_id", "rest_id", "date_time", "num_ppl"]
+        val = ""
+        id_r = 0
+        id_u = 0
+        d = 0
+        num_ppl = 1
+        for i in arr_of_cols:
+
+            val = request.POST.get(i)
+
+            if val is None:
+                err = {"errors": i, "reasons": "required"}
+                return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+            #catching incorrect values
+            if i == "user_id":
+                id_u, err, code = book_insert_usr(i, val)
+            elif i == "rest_id":
+                id_r, err, code = book_insert_rest(i, val)
+            elif i == "date_time":
+                d, err, code = book_insert_date(i, val)
+            else:
+                num_ppl, err, code = book_insert_ppl(i, val)
+            if err:
+                return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=code)
+
+        par = Bookings(user_id=Users.objects.get(id_users=id_u), rest_id=Restaurants.objects.get(id_restaurants=id_r),
+                       date_time=d, num_ppl=num_ppl)
+        par.save()
+        order_by = "id_bookings"
+        p = Bookings.objects.values("id_bookings").order_by((F(order_by)).desc(nulls_last=True)).first()
+        dictionary = {}
+        dictionary['items'] = p
+
+        return JsonResponse(dictionary, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=405)
+
+@csrf_exempt
+def book_edit(request, book_id): #edit a booking
+    if request.method == 'PUT':
+        if request.GET.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        arr_of_cols = ["date_time", "num_ppl", "rest_id", "user_id"]
+        val = book_id
+        for c in arr_of_cols:
+            to_update = request.GET.get(c)
+            if to_update is not None:
+
+                if c == "user_id":
+                    id_u, err, code = book_insert_usr(c, to_update)
+                elif c == "rest_id":
+                    id_r, err, code = book_insert_rest(c, to_update)
+                elif c == "date_time":
+                    d, err, code = book_insert_date(c, to_update)
+                else:
+                    num_ppl, err, code = book_insert_ppl(c, to_update)
+                if err:
+                    return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=code)
+
+                a = {c: to_update}
+                Bookings.objects.filter(id_bookings=val).update(**a)
+        return JsonResponse({}, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=405)
+
+
+@csrf_exempt
+def book_del(request, book_id): #delete a booking
+    if request.method == 'DELETE':
+        if request.GET.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        err, err_code = for_key_const("id_bookings", book_id)
+        if err_code == 2:
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+        Bookings.objects.get(id_bookings=book_id).delete()
+        return JsonResponse({}, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+
+
+@csrf_exempt
+def book_get(request, user_id): #list all users bookings
+    if request.method == 'GET':
+        if request.GET.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        q = Bookings.objects.values("id_bookings", "date_time", "num_ppl", 'rest_id__restaurant_name').filter(user_id=user_id)
+        to_dict = []
+        for i in q:
+            to_dict.append(i)
+
+        dictionary = {}
+        dictionary['items'] = to_dict
+
+        return JsonResponse(dictionary, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+
+
+
+@csrf_exempt
+def book_det(request, book_id): #get info about a booking
+    if request.method == 'GET':
+        if request.GET.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        p = Bookings.objects.values("id_bookings", "date_time", "num_ppl", 'rest_id__restaurant_name').filter(id_bookings=book_id).first()
+
+        dictionary = {}
+        dictionary['items'] = p
+
+        return JsonResponse(dictionary, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+@csrf_exempt
+def book_delall(request): #delete all bookings of a user before a specified date
+    if request.method == 'DELETE':
+        if request.GET.get("dbs_psswd") != dbs_psswd:
+            err = {"errors": "dbs_psswd", "reasons": "incorrect"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=401)
+        arr_of_cols = ["user_id", "date_time"]
+        val1 = ""
+        val2 = ""
+        for i in arr_of_cols:
+
+            val = request.GET.get(i)
+            if val is None:
+                err = {"errors": i, "reasons": "required"}
+                return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
+            if i == "user_id":
+                val1 = val
+            if i == "date_time":
+                val2 = val
+        try:
+            Bookings.objects.filter(user_id=val1, date_time__lte=val2).delete()
+
+        except:
+            err = {"errors": "bookings", "reasons": "no such bookings"}
+            return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+
+        return JsonResponse({}, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=200)
+
+
+    else:
+        err = {"errors": "method", "reasons": "unrecognized"}
+        return JsonResponse(err, json_dumps_params={'ensure_ascii': False, "indent": 4}, status=422)
